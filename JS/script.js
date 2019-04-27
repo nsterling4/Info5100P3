295bdbcc1c4d1c716e0c0065ed21570be18096ee
Noah Sterling <nas95@cornell.edu>
2019-04-27 13:37:58 -0400

    wealth viz and animations

--- JS/script.js
+++ JS/script.js
@@ -30,6 +30,8 @@ const technologyData = async () => {
     const yearMin = d3.min(wealthData, d => d.TIME); //X Axis
     const yearMax = d3.max(wealthData, d => d.TIME);
 
+
+
     // const yearMin = 1950;
     // const yearMax = 2010;
 
@@ -86,7 +88,7 @@ const technologyData = async () => {
         .call(xGridlines);
 
 
-    let yGridlines = d3.axisLeft(wealthScale).ticks(8).tickSize(-plotWidth - 10).tickFormat("");
+    let yGridlines = d3.axisLeft(wealthScale).ticks(11).tickSize(-plotWidth - 10).tickFormat("");
     svgMain.append("g")
         .attr("class", "y gridlines") // Y axis
         .attr("transform", "translate(" + (svgMainMargin.left) + "," + svgMainMargin.top + ")")
@@ -97,17 +99,30 @@ const technologyData = async () => {
         .attr("transform", "translate(" + svgMainMargin.left + "," + svgMainMargin.top + ")");
 
     wealthData.shift();
+    wealthData.pop();
 
-   // console.log(wealthArray);
+    let zeroArray = [];
+    for (let i=0; i<wealthData.length; i++) {
+        zeroArray.push( [yearScale(wealthData[i].TIME), wealthScale(wealthMin)])
+    }
+
+    let wealthArray = [];
+    for (let i=0; i<wealthData.length; i++) {
+        wealthArray.push( [ yearScale(wealthData[i].TIME), wealthScale(wealthData[i].Value) ] )
+    }
+
     let len = wealthData.length;
-    wealthData.push({TIME:  wealthData[len-1].TIME, Value: wealthMin  });
-    wealthData.push({TIME:  yearMin, Value: wealthMin  });
 
-    // console.log(wealthArray);
+    wealthArray.push([yearScale(wealthData[len-1].TIME), wealthScale(wealthMin)  ]);
+    wealthArray.push([yearScale(yearMin), wealthScale(wealthMin)]);
 
 
-  // 3. Set up d3's line generator
-  var line = d3.line()
+
+  //3. Set up d3's line generator
+
+  var line = d3.line().curve(d3.curveMonotoneX);
+
+  var line2 = d3.line()
         .x(d =>  yearScale(d.TIME))
         .y(d =>  wealthScale(d.Value))
         .curve(d3.curveMonotoneX);
@@ -117,183 +132,20 @@ const technologyData = async () => {
   // Try adding a smoothing function after .x and .y using .curve(func) : .curve(d3.curveMonotoneX), curveStep, curveBasis, curveCardinal
 
 
+
   plot.append("path").attr("class","line")
-      .datum( wealthData ) // datum is the single form of .data -- it sends the whole list of data to one element instead of .data mapping each row to an element
-        // We need to give d3.line() some .datum to start with, so we provide an empty array
-        //  If you already know what your data look like, you can just give it the whole dataset at this point
-      .attr("d", line)
-      .style("opacity",.5)
-      .style("fill", "limegreen");
-      // equivalent to .attr("d", d => line(d)), recall that d3.line() returns a function that takes a datapoint parameter
-
-
-
-//   // 5. Make a home for some circles too!
-//   let circLayer = lineSVG.append("g").attr("transform","translate("+(lineMargin.left)+","+lineMargin.top+")")
-
-
-//   function updateLineChart(mouseData) {
-//     console.log(mouseData);
-
-//     // Update our axes
-//     xLineScale.domain([ 0,mouseData.length-1 ]);
-//     xLineAxis.ticks(mouseData.length-1 );
-
-//     // Use d3.extent to get min and max in one command
-//     // let minMax = d3.extent(mouseData, d => d.mouseY);
-//     // console.log(minMax);
-//     // yLineScale.domain( minMax );
-
-//     // For now, we are going to set the yLineScale domain based on the **whole window** of the browser
-//     yLineScale.domain( [ 0, window.innerHeight ] );
-
-//     // Select the axis <g> tag using a class, then call lineAxis again on them to update. Added a transition() to animate the change
-//     lineSVG.select(".x").transition().duration(300).call(xLineAxis);
-//     lineSVG.select(".y").transition().duration(300).call(yLineAxis);
-
-//     // Make some circles
-//     let circles = circLayer.selectAll("circle").data(mouseData);   // We want to use .data here because we want one circle per row
-
-//     circles.enter().append("circle")
-//       .attr("r",4)
-//       .style("fill","darkblue")
-//       .attr("cx", (d,i) => xLineScale( i ) )
-//       .attr("cy", d => yLineScale( d.mouseY ))
-//       // Chaining in .merge here adds any of the circles circLayer.selectAll found and let's us run functions on all of them together
-//       //  this is good for things you want to UPDATE on existing points and SET for new points
-//       .merge(circles)
-//       .attr("cx", (d,i) => xLineScale( i ) )
-//       .attr("cy", d => yLineScale( d.mouseY ));
-
-//     circles.exit().remove();
-
-//     // Like the axes, we need to get the path to call .line again
-//     //  We want to use .datum here because we want one path for all of the rows
-//     //  We can add .transition() to animate, but it looks odd
-//     lineSVG.select(".line").datum(mouseData).attr("d", line);
-
-
-
-//   }
-
-
-
-
-
-
-
-//   // Bind an event listener to the entire page looking for mouse moves every second
-//   //  Calls updateLineChart with new data if some new moves are detected
-//   var mouseLocations = [];
-//   let tickSize = 1000; //in milliseconds
-//   let totalTimeWindow = 15; //in seconds
-//   let totalTicks = (totalTimeWindow*1000) / tickSize;
-//   document.body.addEventListener('mousemove',function(e)
-//   {
-//     if (mouseLocations.length < 1) {
-
-//       mouseLocations.push( {
-//         "mouseX": e.clientX,
-//         "mouseY": e.clientY,
-//         "time": Math.floor(Date.now() / tickSize)
-//       } );
-//       updateLineChart(mouseLocations);
-
-//     }
-//     else {
-//       t = Math.floor(Date.now() / tickSize);
-
-//       // Check if we've reached a new second
-//       if (mouseLocations[mouseLocations.length-1].time < t) {
-//         mouseLocations.push( {
-//           "mouseX": e.clientX,
-//           "mouseY": e.clientY,
-//           "time": t
-//         } );
-//         if (mouseLocations.length > totalTicks) {
-//           mouseLocations.shift(); // remove last element added
-//         }
-//         updateLineChart(mouseLocations);
-//       }
-
-//     }
-
-//   });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    //     //  Credit Prof. Rz. Adapted as needed
-    //     const legendBox = d3.select("#usaMapLegend");
-    //     const legendBoxWidth = legendBox.attr("width");
-    //     const legendBoxHeight = legendBox.attr("height");
-    //     const barHeight = 25;
-    //     const stepSize = 5;
-
-
-    //     let legendMargin = {
-    //         top: 40,
-    //         right: 10,
-    //         bottom: 20,
-    //         left: 10
-    //     };
-    //     const legendWidth = legendBoxWidth - legendMargin.left - legendMargin.right;
-    //     const legendHeight = legendBoxHeight - legendMargin.top - legendMargin.bottom;
-
-
-    //     const pixelScale = d3.scaleLinear()
-    //         .domain([0, legendWidth])
-    //         .range([minMax[0], minMax[1]]); // In this case the "data" are pixels, and we get numbers to use in colorScale
-    //     const barScale = d3.scaleLinear()
-    //         .domain([minMax[0], minMax[1]])
-    //         .range([0, legendWidth]);
-    //     const barAxis = d3.axisBottom(barScale);
-
-
-    //     legendBox.html("");
-
-
-    //     legendBox.append("g")
-    //         .attr("class", "legendAxis")
-    //         .style("stroke", "white")
-    //         .style("stroke-width", ".5px")
-    //         .attr("transform", "translate(" + legendMargin.left + "," + legendMargin.top + ")")
-    //         .call(barAxis);
-
-    //     // Draw rects of color down the bar
-    //     let bar = legendBox.append("g").attr("transform", "translate(" + (10) + "," + (10) + ")")
-    //     for (let i = 0; i < legendWidth; i = i + stepSize) {
-    //         bar.append("rect")
-    //             .attr("x", i)
-    //             .attr("y", 0)
-    //             .attr("width", stepSize)
-    //             .attr("height", barHeight)
-    //             .style("fill", colorScale(pixelScale(i))); // pixels => countData => color
-    //     }
-
-    // }
-
-
-
-
-
-
+  .attr('d', line(zeroArray))
+  .style("opacity",.5)
+  .style("fill", "limegreen")
+  .transition()
+  .attr('d', line(wealthArray))
+  .delay(500).duration(2000).ease(d3.easeBackOut)
+  
+  .on("end",function() { // on end of transition...
+    d3.select(".y")
+        .transition() // second transition
+           // .style("opacity", 0);
+});;
 
 
 }
